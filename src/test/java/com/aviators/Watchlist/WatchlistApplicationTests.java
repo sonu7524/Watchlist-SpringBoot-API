1ff7a4a01aa2c6dbd9ad3351ef00e640080fd5ed
sonu7524 <sonudaryani135@gmail.com>
2023-01-27 15:15:57 +0530

    First RestFul Api

---
+++ src/test/java/com/aviators/Watchlist/WatchlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aviators.Watchlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
